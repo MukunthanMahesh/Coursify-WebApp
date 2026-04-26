f28dedd979dedfc27b50088ccf9a7b71435d7b68
Amaan Javed <amaanjaved2004@gmail.com>
2026-04-26 12:50:09 -0400

    refactor: rename Navigation and Footer to kebab-case

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { type CSSProperties } from "react";
 import Link from "next/link";
-import Footer from "@/components/Footer";
+import Footer from "@/components/footer";
 import {
   Brain,
   BarChart,
